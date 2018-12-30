e0b6fe57a93f04047e5625abc44ff3bd62a8d9e1
侯坤林 <houkunlin@ibona.cn>
2018-12-30 20:08:22 +0800

    添加一个json数据返回实体类

---
+++ src/main/kotlin/cn/goour/model/Msg.kt
@@ -0,0 +1,25 @@
+package cn.goour.model
+
+data class Msg(
+    var code: Int,
+    var message: String,
+    var data: Any? = null
+) {
+    companion object {
+        fun success(): Msg {
+            return success("success")
+        }
+
+        fun success(message: String): Msg {
+            return Msg(0, message)
+        }
+
+        fun error(): Msg {
+            return error("error")
+        }
+
+        fun error(message: String): Msg {
+            return Msg(1, message)
+        }
+    }
+}
